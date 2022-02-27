0b22f6477cfc25a4803ac7d35e30c074b9789ae7
Bulat Valiakhmetov <61677060+Bulatv2@users.noreply.github.com>
2022-02-27 06:11:13 +0500

    Update classifier.py

--- classifier.py
+++ classifier.py
@@ -12,12 +12,18 @@ with open("classes.txt", "r") as file:
             left, right, *res = line.split(":")
             llist.append(left)
             rlist.append(right)
-class Classifier:
+class Classificator:
     def __init__(self, text):
         self.text = text
     def load(self):
         for i in self.text:
+            flag = 1
             for j in llist:
                 if i == j:
+                    classlist.append(i)
                     classlist.append(rlist[llist.index(j)])
+                    flag = 0
+                    break
+            if flag != 0:
+                classlist.append(i)
         return classlist
