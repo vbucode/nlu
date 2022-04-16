68e45e047c1b4e0bbbed4987ce2d2c93585c323f
vbucode <61677060+vbucode@users.noreply.github.com>
2022-04-16 13:18:21 +0500

    Add files via upload

--- nerclassifier.py
+++ nerclassifier.py
@@ -23,32 +23,44 @@ class NerClassificator:
                 var -= 1
                 break
             else:
-                for j in llist:
-                    count += 1
-                    clearlist = []
-                    clearlist = re.split("[\-\s]", j)
-                    if clearlist[0] == i and len(clearlist) == 1:
-                        nerlist.append((i, rlist[llist.index(j)].replace("\n", "")))
-                        break
-                    elif clearlist[0] == i and len(clearlist) > 1:
-                        count2 = 0
+                try:
+                    if i.isnumeric():
                         triallist = []
-                        for k in clearlist:
-                            count2 += 1
-                            if k == self.text[self.text.index(i) + count2 - 1]:
-                                triallist.append([k, rlist[llist.index(j)].replace("\n", "")])
-                        if len(triallist) == len(clearlist):
+                        triallist.append([i, "date"])
+                        if rlist[llist.index(self.text[self.text.index(i) + 1])].replace("\n", "") == "date":
+                            triallist.append([self.text[self.text.index(i) + 1], "date"])
                             for n in triallist:
-                                if triallist.index(n) == 0:
-                                    nerlist.append((n[0], "b-" + n[1]))
-                                elif triallist.index(n) == len(triallist) - 1:
-                                    nerlist.append((n[0], "i-" + n[1]))
-                                elif triallist.index(n) != 0 and triallist.index(n) != -1:
-                                    nerlist.append((n[0], n[1]))
-                            var = len(clearlist) - 1
-                        else:
+                                nerlist.append((n[0], n[1]))
+                            var = len(triallist) - 1
+                except ValueError:
+                    nerlist.append((i, "out"))
+                else:
+                    for j in llist:
+                        count += 1
+                        clearlist = []
+                        clearlist = re.split("[\-\s]", j)
+                        if clearlist[0] == i and len(clearlist) == 1:
                             nerlist.append((i, rlist[llist.index(j)].replace("\n", "")))
-                        break
-                    elif count == len(llist):
-                        nerlist.append((i, "out"))
+                            break
+                        elif clearlist[0] == i and len(clearlist) > 1:
+                            count2 = 0
+                            triallist = []
+                            for k in clearlist:
+                                count2 += 1
+                                if k == self.text[self.text.index(i) + count2 - 1]:
+                                    triallist.append([k, rlist[llist.index(j)].replace("\n", "")])
+                            if len(triallist) == len(clearlist):
+                                for n in triallist:
+                                    if triallist.index(n) == 0:
+                                        nerlist.append((n[0], "b-" + n[1]))
+                                    elif triallist.index(n) == len(triallist) - 1:
+                                        nerlist.append((n[0], "i-" + n[1]))
+                                    elif triallist.index(n) != 0 and triallist.index(n) != -1:
+                                        nerlist.append((n[0], n[1]))
+                                var = len(clearlist) - 1
+                            else:
+                                nerlist.append((i, rlist[llist.index(j)].replace("\n", "")))
+                            break
+                        elif count == len(llist):
+                            nerlist.append((i, "out"))
         return nerlist
