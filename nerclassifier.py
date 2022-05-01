f43de209ebcb24224c27945620742a07b1cd035e
vbucode <61677060+vbucode@users.noreply.github.com>
2022-05-01 18:16:44 +0500

    Add files via upload

--- nerclassifier.py
+++ nerclassifier.py
@@ -12,6 +12,9 @@ with open("ner-ru.txt", "r") as file:
             left, right, *res = line.split(":")
             llist.append(left)
             rlist.append(right.replace("\n", ""))
+
+month = ["январь", "января", "октябрь", "октября", "года"]
+
 class NerClassificator:
     def __init__(self, text):
         self.text = text
@@ -23,31 +26,23 @@ class NerClassificator:
                 var -= 1
                 break
             else:
-                try:
-                    if i.isnumeric():
-                        triallist = []
-                        if rlist[llist.index(self.text[self.text.index(i) + 1])] == "date":
-                            triallist.append([i, "date"])
-                            triallist.append([self.text[self.text.index(i) + 1], "date"])
-                            for n in triallist:
-                                if triallist.index(n) == 0:
-                                    nerlist.append((n[0], "b-" + n[1]))
-                                elif triallist.index(n) == len(triallist) - 1:
-                                    nerlist.append((n[0], "i-" + n[1]))
-                                elif triallist.index(n) != 0 and triallist.index(n) != -1:
-                                    nerlist.append((n[0], n[1]))
-                            var = len(triallist) - 1
-                except ValueError:
-                    nerlist.append((i, "out"))
-                if i.isnumeric() == False:
+                if i.isnumeric() and int(i):
+                    flag = 0
+                    for k in month:
+                        if k == self.text[self.text.index(i) + 1]:
+                            nerlist.append((i, "date"))
+                            flag = 1
+                    if flag == 0:
+                        nerlist.append((i, "out"))
+                elif i.isnumeric() == False:
                     for j in llist:
                         count += 1
                         clearlist = []
                         clearlist = re.split("[\-\s]", j)
-                        if clearlist[0] == i and len(clearlist) == 1:
+                        if i == clearlist[0] and len(clearlist) == 1:
                             nerlist.append((i, rlist[llist.index(j)]))
                             break
-                        elif clearlist[0] == i and len(clearlist) > 1:
+                        elif i == clearlist[0] and len(clearlist) > 1:
                             count2 = 0
                             triallist = []
                             for k in clearlist:
@@ -65,7 +60,7 @@ class NerClassificator:
                                 var = len(clearlist) - 1
                             else:
                                 nerlist.append((i, rlist[llist.index(j)]))
-                            break
+                                break
                         elif count == len(llist):
-                            nerlist.append((i, "out"))
+                             nerlist.append((i, "out"))
         return nerlist
